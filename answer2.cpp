82e58d2ecbcf92b9b4710a6728b613499e127d65
ram495 <38049954+ram495@users.noreply.github.com>
2018-04-05 07:13:12 -0700

    Add files via upload

---
+++ answer2.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	char a[n+1];
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	string s;
+	cin>>s;
+	func(a[n+1],s);
+	
+}
+int func(char a[],string s)
+{    char s =a[0];
+     int m;
+     string g[n];
+     vector<string>b[0];
+	 for(int j=0;j<n;j++)
+	{
+	   	if((int)a[i]<(int)s)
+	   	{
+	   		
+		}
+	}
+	
+	
+return m;	
+}
